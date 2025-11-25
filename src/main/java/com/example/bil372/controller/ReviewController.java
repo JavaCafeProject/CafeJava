e46b6bb37bd2653bc853f38773882a2e89b3693c
SeydaSu <ssudupak711@gmail.com>
2025-11-25 22:29:58 +0300

    feat(Review): add review service and DTOs

--- src/main/java/com/example/bil372/controller/ReviewController.java
+++ src/main/java/com/example/bil372/controller/ReviewController.java
@@ -1,7 +1,28 @@
 package com.example.bil372.controller;
 
+import com.example.bil372.dto.request.ReviewRequest;
+import com.example.bil372.dto.response.ReviewResponse;
+import com.example.bil372.model.Review;
+import com.example.bil372.service.IReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/review")
 public class ReviewController {
+
+    private final IReviewService reviewService;
+
+    @PostMapping
+    public ReviewResponse createReview(@RequestBody ReviewRequest review){
+        return reviewService.createReview(review);
+    }
+
+    @GetMapping("/item/{id}")
+    public ReviewResponse getReviewById(@PathVariable Long id){
+        return reviewService.getReviewById(id);
+    }
 }

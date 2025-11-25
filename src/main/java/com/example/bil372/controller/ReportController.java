9266a2c5c7fc3521d97051b7a5fe7979c2c1fa9c
SeydaSu <ssudupak711@gmail.com>
2025-11-25 22:30:23 +0300

    feat(Report): add report service and DTOs

--- src/main/java/com/example/bil372/controller/ReportController.java
+++ src/main/java/com/example/bil372/controller/ReportController.java
@@ -1,7 +1,44 @@
 package com.example.bil372.controller;
 
+import com.example.bil372.dto.report.DailySalesReport;
+import com.example.bil372.dto.report.EmployeePerformanceReport;
+import com.example.bil372.dto.report.RevenueReport;
+import com.example.bil372.dto.report.TopProductReport;
+import com.example.bil372.dto.response.ReviewResponse;
+import com.example.bil372.service.IReportService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/report")
 public class ReportController {
+
+    private final IReportService reportService;
+
+    @GetMapping("/daily-sales")
+    public DailySalesReport getDailySalesReport() {
+        return reportService.getDailySalesReport();
+    }
+
+    @GetMapping("/top-products")
+    public List<TopProductReport> getTopProducts() {
+        return reportService.getTopProducts();
+    }
+
+    @GetMapping("/employee-performance")
+    public List<EmployeePerformanceReport> getEmployeePerformance() {
+        return reportService.getEmployeePerformance();
+    }
+
+    @GetMapping("/revenue")
+    public RevenueReport getRevenueReport() {
+        return reportService.getRevenueReport();
+    }
 }
--- src/main/java/com/example/bil372/repository/OrderRepository.java
+++ src/main/java/com/example/bil372/repository/OrderRepository.java
@@ -1,9 +1,56 @@
 package com.example.bil372.repository;
 
+import com.example.bil372.dto.report.DailySalesReport;
+import com.example.bil372.dto.report.EmployeePerformanceReport;
+import com.example.bil372.dto.report.RevenueReport;
 import com.example.bil372.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+
+    @Query(value = """
+    SELECT 
+        CURDATE(),
+        COUNT(*),
+        COALESCE(SUM(o.total_price), 0)
+    FROM orders o
+    WHERE o.order_date BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 1 DAY)
+    """, nativeQuery = true)
+    Object[] getDailySales();
+
+
+
+
+    @Query("""
+        SELECT e.Id, e.firstName, e.lastName, COUNT(o.id), COALESCE(SUM(o.totalPrice), 0)
+        FROM Order o
+        JOIN o.employee e
+        WHERE e IS NOT NULL
+        GROUP BY e.Id, e.firstName, e.lastName
+        ORDER BY COALESCE(SUM(o.totalPrice), 0) DESC
+    """)
+    List<Object[]> getEmployeePerformance();
+
+
+
+    @Query("""
+        SELECT 
+            COALESCE(SUM(o.totalPrice), 0),
+            COALESCE(SUM(CASE WHEN FUNCTION('DATE', o.orderDate) = CURRENT_DATE THEN o.totalPrice ELSE 0 END), 0)
+        FROM Order o
+    """)
+    Object[] getRevenue();
+
+
+
+
 }
